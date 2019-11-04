5929a9ab1d9b87e67d28bfc2dd5fdd3473080cbb
Thomas Höll <thomas.hoell@neos-it.de>
2019-11-04 10:41:31 +0100

    [fixed] tweaked S3 upload so that timeouts and connection resets are (hopefully) now avoided [added] print size of upload and upload rate
    Signed-off-by: Thomas Höll <thomas.hoell@neos-it.de>

--- storage/s3.go
+++ storage/s3.go
@@ -7,10 +7,13 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/hako/durafmt"
 	"github.com/huacnlee/gobackup/logger"
+	"math"
 	"net/http"
 	"os"
 	"path"
+	"time"
 )
 
 // S3 - Amazon S3 storage
@@ -31,17 +34,22 @@ type S3 struct {
 }
 
 func (ctx *S3) open() (err error) {
-	ctx.viper.SetDefault("region", "us-east-1")
+	ctx.viper.SetDefault("region", "eu-central-1")
+	ctx.viper.SetDefault("max_retries", 3)
+
 	cfg := aws.NewConfig()
 	endpoint := ctx.viper.GetString("endpoint")
+
 	if len(endpoint) > 0 {
 		cfg.Endpoint = aws.String(endpoint)
 	}
+
 	cfg.Credentials = credentials.NewStaticCredentials(
 		ctx.viper.GetString("access_key_id"),
 		ctx.viper.GetString("secret_access_key"),
 		ctx.viper.GetString("token"),
 	)
+
 	cfg.Region = aws.String(ctx.viper.GetString("region"))
 	cfg.MaxRetries = aws.Int(ctx.viper.GetInt("max_retries"))
 
@@ -52,7 +60,12 @@ func (ctx *S3) open() (err error) {
 		cfg.S3ForcePathStyle = aws.Bool(true)
 	}
 
-	httpClient := &http.Client{Timeout:0}
+	duration, err := time.ParseDuration("4h")
+	if err != nil {
+		return fmt.Errorf("failed to parse timeout duration %v", err)
+	}
+
+	httpClient := &http.Client{Timeout: duration}
 	cfg.HTTPClient = httpClient
 
 	sess := session.Must(session.NewSession(cfg))
@@ -69,6 +82,13 @@ func (ctx *S3) upload(fileKey string) (err error) {
 		return fmt.Errorf("failed to open file %q, %v", ctx.archivePath, err)
 	}
 
+	defer f.Close()
+
+	info, err := f.Stat()
+	if err != nil {
+		return fmt.Errorf("failed to get size of file %q, %v", ctx.archivePath, err)
+	}
+
 	remotePath := path.Join(ctx.path, fileKey)
 
 	input := &s3manager.UploadInput{
@@ -77,13 +97,38 @@ func (ctx *S3) upload(fileKey string) (err error) {
 		Body:   f,
 	}
 
-	logger.Info("-> S3 Uploading...")
-	result, err := ctx.client.Upload(input)
+	logger.Info(fmt.Sprintf("-> S3 Uploading (%d MiB)...", info.Size()/(1024*1024)))
+
+	start := time.Now()
+
+	result, err := ctx.client.Upload(input, func(uploader *s3manager.Uploader) {
+		// set the part size as low as possible to avoid timeouts and aborts
+		// also set concurrency to 1 for the same reason
+		var partSize int64 = 5242880 // 5MiB
+		maxParts := math.Ceil(float64(info.Size() / partSize))
+
+		// 10000 parts is the limit for AWS S3. If the resulting number of parts would exceed that limit, increase the
+		// part size as little as needed but as little possible
+		if maxParts > 10000 {
+			partSize = int64(math.Ceil(float64(info.Size() / 10000)))
+		}
+
+		uploader.Concurrency = 1
+		uploader.LeavePartsOnError = false
+		uploader.PartSize = partSize
+	})
 	if err != nil {
 		return fmt.Errorf("failed to upload file, %v", err)
 	}
 
+	t := time.Now()
+	elapsed := t.Sub(start)
+
 	logger.Info("=>", result.Location)
+	rate := math.Ceil(float64(info.Size()) / (elapsed.Seconds() * 1024 * 1024))
+
+	logger.Info(fmt.Sprintf("Duration %v, rate %.1f MiB/s", durafmt.Parse(elapsed).LimitFirstN(2).String(), rate))
+
 	return nil
 }
 
